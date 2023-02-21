b9070068b175c6abf803275a38fb57a5b964ebdc
MathePLay <Matheus.doer@gmail.com>
2023-02-21 10:20:12 -0300

    Final da aula 101

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,9 +1,14 @@
 import { createContext, ReactNode, useState } from 'react';
+import { destroyCookie, setCookie, parseCookies } from 'nookies';
+import Router from 'next/router';
+
+import { api } from '../services/apiClient';
 
 type AuthContextData = {
   user: UserProps;
   isAuthenticated: boolean;
   signIn: (credentials: SignInProps) => Promise<void>;
+  singOut: () => void;
 }
 
 type UserProps = {
@@ -23,17 +28,52 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData)
 
-export function AuthProvider({ children }: AuthProviderProps){
+export function singOut() {
+  try {
+    destroyCookie(undefined, '@playpizzas.token')
+    Router.push('/')
+  } catch {
+    console.log('Erro ao deslogar')
+  }
+}
+
+export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<UserProps>()
   const isAuthenticated = !!user;
 
-  async function signIn({email, password}: SignInProps){
-    console.log(email)
-    console.log(password)
-  }
+  async function signIn({ email, password }: SignInProps) {
+    try {
+      const response = await api.post('/session', {
+        email,
+        password
+      })
+      // console.log(response.data)
 
-  return(
-    <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
+      const { id, name, token } = response.data;
+      setCookie(undefined, '@playpizzas.token', token, {
+        maxAge: 60*60*24*30, // Expirar em 1 mes
+        path: "/", // Quais caminhos terrão acceso ao cookie
+      })
+
+      setUser({
+        id,
+        name,
+        email,
+      })
+
+      //Passar para proximas requisiçoes o nosso token
+      api.defaults.headers['Authorization'] = `Bearer ${token}`
+
+      // Redirecionar o user para /dashboard
+      Router.push('/dashboard')
+
+
+    } catch (err) {
+      console.log("Erro ao Acessar ", err)
+    }
+  }
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, signIn, singOut }}>
       {children}
     </AuthContext.Provider>
   )
---
+++ frontend/src/pages/dashboard/index.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard(){
+    return(
+        <div>
+            <h1>Deashboard Page</h1>
+        </div>
+    )
+}
