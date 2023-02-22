442aba5298c5e044951674d21e515ecead01bcaa
Matheus Doerzbacher <Matheus.doer@gmail.com>
2023-02-22 13:16:24 -0300

    Final da aula 117

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -6,9 +6,14 @@ import style from './styles.module.scss';
 
 import { Header } from "../../components/Header"
 
-import {FiRefreshCcw} from 'react-icons/fi'
+import { FiRefreshCcw } from 'react-icons/fi'
 
 import { setupAPIClient } from "@/src/services/api";
+
+import Modal from 'react-modal';
+import { StringifyOptions } from "querystring";
+
+import { ModalOrder } from '../../components/ModalOrder'
 
 type OrderProps = {
     id: string;
@@ -18,47 +23,93 @@ type OrderProps = {
     name: string | null;
 }
 
-interface HomeProps{
+interface HomeProps {
     orders: OrderProps[];
 }
 
-export default function Dashboard({orders}: HomeProps){
-const [orderList, setOrderList] = useState(orders || [])
+export type OrderItemProps = {
+    id: string;
+    amount: number;
+    order_id: string;
+    product_id: string
+    product: {
+        id: string;
+        name: string;
+        description: string;
+        price: string;
+        banner: string;
+    }
+    order: {
+        id: string;
+        table: string | number;
+        status: boolean;
+        name: string | null;
+    }
 
-function handleOpenModalView(id: string){
-    alert("ID CLICADO " + id)
 }
 
-    return(
+export default function Dashboard({ orders }: HomeProps) {
+    const [orderList, setOrderList] = useState(orders || [])
+
+    const [modal, setModalItem] = useState<OrderItemProps[]>()
+    const [modalVisible, setModalVisible] = useState(false)
+
+    function handleCloseModal() {
+        setModalVisible(false);
+    }
+
+    async function handleOpenModalView(id: string) {
+
+        const apiClient = setupAPIClient();
+
+        const response = await apiClient.get('/order/detail', {
+            params: {
+                order_id: id,
+            }
+        })
+
+        setModalItem(response.data);
+        setModalVisible(true)
+    }
+
+
+
+
+    Modal.setAppElement('#__next');
+    return (
         <>
-        <Head>
-            <title>Painel - Play Pizzas</title>
-        </Head>
-        <div>
-            <Header/>
+            <Head>
+                <title>Painel - Play Pizzas</title>
+            </Head>
+            <div>
+                <Header />
 
-            <main className={style.container}>
-                <div className={style.containerHeader}>
-                    <h1>Ultimos Pedidos</h1>
-                    <button>
-                        <FiRefreshCcw size={25} color="#efffa3"/>
-                    </button>
-                </div>
+                <main className={style.container}>
+                    <div className={style.containerHeader}>
+                        <h1>Ultimos Pedidos</h1>
+                        <button>
+                            <FiRefreshCcw size={25} color="#efffa3" />
+                        </button>
+                    </div>
 
-                <article className={style.listOrder}>
-                    {orderList.map(item => (
-                        <section key={item.id} className={style.orderItem}>
-                            <button onClick={() => handleOpenModalView(item.id) }>
-                                <div className={style.tag}></div>
-                                <span>Mesa {item.table}</span>
-                            </button>
-                        </section>
-                    ))}
-                    
+                    <article className={style.listOrder}>
+                        {orderList.map(item => (
+                            <section key={item.id} className={style.orderItem}>
+                                <button onClick={() => handleOpenModalView(item.id)}>
+                                    <div className={style.tag}></div>
+                                    <span>Mesa {item.table}</span>
+                                </button>
+                            </section>
+                        ))}
 
-                </article>
-            </main>
-        </div>
+
+                    </article>
+                </main>
+
+                {modalVisible && (
+                    <ModalOrder/>
+                )}
+            </div>
         </>
     )
 }
@@ -70,7 +121,7 @@ export const getServerSideProps = canSSRAuth(async (ctx) => {
 
 
 
-    return{
+    return {
         props: {
             orders: responde.data
         }
